242362cce95c574b577f9f308e0187494d0dcabf
Marko Gvero <mgvero@ivytech.edu>
2026-07-23 16:32:39 -0500

    Add authentication and comments

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
-from .models import Post
-from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+from .models import Comment, Post
+from .forms import CommentForm, PostForm
 
 def post_list(request):
     posts = Post.objects.filter(
@@ -9,6 +10,18 @@ def post_list(request):
     ).order_by('published_date')
 
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(
+        published_date__isnull=True
+    ).order_by('created_date')
+
+    return render(
+        request,
+        'blog/post_draft_list.html',
+        {'posts': posts}
+    )
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -19,6 +32,7 @@ def post_detail(request, pk):
         {'post': post}
     )
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -38,6 +52,7 @@ def post_new(request):
         {'form': form}
     )
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
 
@@ -58,3 +73,55 @@ def post_edit(request, pk):
         'blog/post_edit.html',
         {'form': form}
     )
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+
+    return redirect('post_detail', pk=post.pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == "POST":
+        post.delete()
+
+    return redirect('post_list')
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+
+    return render(
+        request,
+        'blog/add_comment_to_post.html',
+        {'form': form}
+    )
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+
+    return redirect('post_detail', pk=post_pk)
--- mysite/settings.py
+++ mysite/settings.py
@@ -117,3 +117,5 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
+
+LOGIN_REDIRECT_URL = '/'
